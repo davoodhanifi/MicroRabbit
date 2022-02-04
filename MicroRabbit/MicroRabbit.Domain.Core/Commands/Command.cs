a553f3a5ec4f6460cbc3dc43d5d05b7d6a556440
davood.hanifi <davood.hanifi@gmail.com>
2022-02-04 13:47:16 +0330

    Implemented interfaces and classes for Domain.Core.

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime DateTime
+        {
+            get;
+            protected set;
+        }
+
+        public Command()
+        {
+            DateTime = DateTime.Now;
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime dateTime
+        {
+            get;
+            protected set;
+        }
+
+        public Event()
+        {
+            dateTime = DateTime.Now;
+        }
+    }
+}
